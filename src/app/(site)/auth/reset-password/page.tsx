e9217c11983e1fd70e51a554d4fcb0a09c1f144c
sunshine-web-coder <exydevcoder@gmail.com>
2025-08-30 22:13:55 +0100

    dfdf

--- src/app/(site)/auth/reset-password/page.tsx
+++ src/app/(site)/auth/reset-password/page.tsx
@@ -1,6 +1,9 @@
 import ResetPasswordForm from "@/components/auth/ResetPasswordForm";
 import React from "react";
 
+// Force dynamic rendering to prevent prerender errors
+export const dynamic = 'force-dynamic'
+
 export default function ResetPasswordPage() {
   return <ResetPasswordForm />;
 }
